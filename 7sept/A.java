e12e81cdbda5f183ea9e8fafa7553cb8d1d6bee5
sweta12-s <82711225+sweta12-s@users.noreply.github.com>
2022-09-21 12:56:30 +0530

    Add files via upload

---
+++ 7sept/A.java
@@ -0,0 +1,31 @@
+/* program : Average of 3 subjects
+@author : Shuruti Singhal
+@Date : 6 sept 2022
+*/
+
+// declaring a class
+class A
+{
+	
+	
+	// calling main
+	public static void main(String[] args)
+	{
+		int number=7;
+		System.out.println("Number is :"+number);
+	}
+}
+
+
+class LiteralDemo
+{
+	// calling main
+	
+	public static void main(String[] args)
+	{
+		int number=90;
+		System.out.println("Number is :"+number);
+	}
+	
+}
+
---
+++ 7sept/VersionDemo.java
@@ -0,0 +1,20 @@
+/* program : Current version of java
+@author : Shuruti Singhal
+@Date : 6 sept 2022
+*/
+
+// declaring a class
+class VersionDemo
+{
+	
+	
+	// calling main
+	public static void main(String[] args)
+	{
+		System.out.println(System.getProperty("java.version"));
+	}
+	
+	// end of main
+}
+
+// end of class VersionDemo
