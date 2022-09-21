f659666bcab3ac633b5935ce2b4c534c11b4bd3a
sweta12-s <82711225+sweta12-s@users.noreply.github.com>
2022-09-21 12:54:06 +0530

    Add files via upload

---
+++ 5Sep/FirstDemo.java
@@ -0,0 +1,20 @@
+/*
+Program : This is the first java Program
+@author: Shuruti Singhal
+@Date:
+5sept2022
+*/
+
+//declaring class
+class FirstDemo
+{
+	//calling main method
+	public static void main(String args[])
+	{
+		System.out.println("Hello World"); // printing hello world
+	}
+	
+	//end of main
+	
+//end of class FirstDemo
+}
